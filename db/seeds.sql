32bce29a4e3fedb550206b5ff20e52832fc6dbd8
Paris Cornett <pariscornett@gmail.com>
2019-08-02 00:33:48 -0500

    created schema and seed sql files :sunglasses:

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Mushroom Swiss Burger");
+INSERT INTO burgers (burger_name) VALUES ("Stroganoff Burger");
+INSERT INTO burgers (burger_name) VALUES ("Goat Cheese and Sundried Tomato Burger");
---
+++ server.js
@@ -0,0 +1,4 @@
+//set up dependencies
+var express = require("express");
+
+var app = express();
