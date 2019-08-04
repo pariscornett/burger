ba1aae4d93f9c5a01c578b595c03022ce166e0a9
Paris Cornett <pariscornett@gmail.com>
2019-08-03 22:38:22 -0500

    added port and listener

--- server.js
+++ server.js
@@ -2,3 +2,11 @@
 var express = require("express");
 
 var app = express();
+
+var PORT = port.env.PORT || 8080;
+
+
+//listener 
+app.listen(PORT, function() {
+    console.log("App listening on PORT: http://localhost:" + PORT);
+};
